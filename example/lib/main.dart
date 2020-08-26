ad6c8b5a3c2fbb30637f481e7ce834caea17588d
Ju Hyuk Park <jpark@roo.kr>
2020-08-26 15:41:59 +0900

    Android implementation is in progress.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  List<PairedDevice> _pairedDevices = [];
 
   @override
   void initState() {
@@ -23,21 +23,12 @@ class _MyAppState extends State<MyApp> {
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterBrotherPrinter.getPairedDevices;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    // setState(() {
+    //   _platformVersion = platformVersion;
+    // });
   }
 
   @override
@@ -48,7 +39,51 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            children: [
+              MaterialButton(
+                onPressed: () async {
+                  await FlutterBrotherPrinter.pairedDevices;
+                },
+                padding: EdgeInsets.all(20),
+                child: Text('Paired Devices'),
+              ),
+
+              (_pairedDevices != null && _pairedDevices.length > 0) ?
+                ListView.builder(
+                  itemCount: _pairedDevices.length,
+                  itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text("Device $index"),
+                        );
+                      },
+                ) : Container(
+                child: Text("Could not retrieve any paired device"),
+              ),
+
+              MaterialButton(
+                onPressed: () async {
+                  await FlutterBrotherPrinter.disconnect();
+                },
+                padding: EdgeInsets.all(20),
+                child: Text('Start Session - connect()'),
+              ),
+              MaterialButton(
+                onPressed: () async {
+                  await FlutterBrotherPrinter.print();
+                },
+                padding: EdgeInsets.all(20),
+                child: Text('Print'),
+              ),
+              MaterialButton(
+                onPressed: () async {
+                  await FlutterBrotherPrinter.disconnect();
+                },
+                padding: EdgeInsets.all(20),
+                child: Text('End Session - disconnect()'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/flutter_brother_printer.dart
+++ lib/flutter_brother_printer.dart
@@ -1,43 +1,48 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/services.dart';
 
 class FlutterBrotherPrinter {
   static const MethodChannel _channel = const MethodChannel('flutter_brother_printer');
 
-  static Future<String> get configure async {
-    // TODO: Not Implemented
-    final String version = await _channel.invokeMethod('configure');
-    return version;
-  }
-
-  static Future<String> get scanDevice async {
+  static Future<String> scanDevice() async {
     // TODO: Not Implemented
     final String version = await _channel.invokeMethod('scanDevice');
     return version;
   }
 
-  static Future<String> get getPairedDevices async {
+  static Future<List<PairedDevice>> get pairedDevices async {
     // TODO: Not Implemented
-    final String version = await _channel.invokeMethod('getPairedDevices');
+    final String devicesJson = await _channel.invokeMethod('getPairedDevices');
+    // return jsonDecode(devicesJson);
+    return [];
+  }
+
+  static Future<String> configure() async {
+    // TODO: Not Implemented
+    final String version = await _channel.invokeMethod('configure');
     return version;
   }
 
-  static Future<String> get connect async {
+  static Future<String> connect() async {
     // TODO: Not Implemented
     final String version = await _channel.invokeMethod('connect');
     return version;
   }
 
-  static Future<String> get print async {
+  static Future<String> print() async {
     // TODO: Not Implemented
     final String version = await _channel.invokeMethod('print');
     return version;
   }
 
-  static Future<String> get disconnect async {
+  static Future<String> disconnect() async {
     // TODO: Not Implemented
     final String version = await _channel.invokeMethod('disconnect');
     return version;
   }
+}
+
+class PairedDevice {
 }
